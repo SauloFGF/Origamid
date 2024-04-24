823a0f0f498fb1d1ceb94064abad052d76265ed3
Saulo Figueiredo <saulo.fgf21@gmai.com>
2024-04-24 18:13:59 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
-import React from "react"
-import useFetch from "./useFetch"
-
-type Produto = {
-  id: string;
-  nome: string;
-  preco: number;
-  quantidade: number;
-  descricao: string;
-  internacional: boolean;
-}
+import Form from "./Form"
 
 function App() {
-   const [id, setId] = React.useState("p001")
-   const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos/");
-   const produto = useFetch<Produto>(`https://data.origamid.dev/produtos/${id}`)
 
-  return (
-    <section className="flex">
-      <div>
-        {produtos.data && 
-        produtos.data.map((produto) => (
-          <button onClick={() => setId(produto.id)} style={{fontSize: "1rem"}} key={produto.id}>{produto.id}</button>
-        ))}
-      </div>
-      <div>
-        {produto.loading && <>Carregando...</>}
-        {produto.data && (
-          <ul>
-            <li>ID: {produto.data.id}</li>
-            <li>Nome: {produto.data.nome}</li>
-            <li>Preco: {produto.data.preco}</li>
-            <li>Quantidade: {produto.data.quantidade}</li>
-            <li>Descricao: {produto.data.descricao}</li>
-            <li>Internacional: {produto.data.internacional ? 'sim' : 'nao'}</li>
-          </ul>
-        )}
-      </div>
-  </section>
+  return (<div>
+    <Form/>
+  </div>
   )
 }
 
--- src/Input.tsx
+++ src/Input.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 type InputProps = React.ComponentProps<'input'> &{
     label: string;
-    setState: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Input = ({label, setState, id, ...props} : InputProps) => {
+const Input = ({label, id, ...props} : InputProps) => {
   return (
     <div style={{ marginBottom: '1rem'}}>
         <label htmlFor={label}>{label}</label>
-        <input id={id} name={id} onChange={({currentTarget}) => setState(currentTarget.value)} type="text" {...props} />
+        <input id={id} name={id} type="text" {...props} />
     </div>
   )
 }
